767f2a1461121b722ec2d76334b3ef35b3a1c26b
Stefano Corda <stefano.corda@epfl.ch>
2022-04-13 18:45:22 +0200

    rewrite Array 4D

--- app/common/types.hpp
+++ app/common/types.hpp
@@ -272,47 +272,46 @@ public:
   Array4D() : Array4D(0, 0, 0, 0) {}
 
   Array4D(size_t w_dim, size_t z_dim, size_t y_dim, size_t x_dim)
-      : m_x_dim(x_dim), m_y_dim(y_dim), m_z_dim(z_dim), m_w_dim(w_dim),
-        m_buffer(allocate_memory<T>(w_dim * z_dim * y_dim * x_dim),
-                 &free) // shared_ptr with custom deleter that deletes an array
-  {}
-
-  Array4D(std::shared_ptr<T> data, size_t w_dim, size_t z_dim, size_t y_dim,
-          size_t x_dim)
-      : m_x_dim(x_dim), m_y_dim(y_dim), m_z_dim(z_dim), m_w_dim(w_dim),
-        m_buffer(data) {}
+      : m_w_dim(w_dim), m_z_dim(z_dim), m_y_dim(y_dim), m_x_dim(x_dim),
+        m_delete_buffer((w_dim * z_dim * y_dim * x_dim) > 0),
+        m_buffer(allocate_memory<T>(w_dim * z_dim * y_dim * x_dim)) {}
 
   Array4D(T *data, size_t w_dim, size_t z_dim, size_t y_dim, size_t x_dim)
-      : m_x_dim(x_dim), m_y_dim(y_dim), m_z_dim(z_dim), m_w_dim(w_dim),
-        m_buffer(data,
-                 [](T *) {}) // shared_ptr with custom deleter that does nothing
-  {}
+      : m_w_dim(w_dim), m_z_dim(z_dim), m_y_dim(y_dim), m_x_dim(x_dim),
+        m_delete_buffer(false), m_buffer(data) {}
 
   Array4D(const Array4D &other) = delete;
   Array4D &operator=(const Array4D &rhs) = delete;
 
   Array4D(Array4D &&other)
-      : m_x_dim(other.m_x_dim), m_y_dim(other.m_y_dim), m_z_dim(other.m_z_dim),
-        m_w_dim(other.m_w_dim), m_buffer(other.m_buffer) {
+      : m_w_dim(other.w_dim), m_z_dim(other.z_dim), m_y_dim(other.y_dim),
+        m_x_dim(other.x_dim), m_delete_buffer(other.m_delete_buffer),
+        m_buffer(other.m_buffer) {
     other.m_buffer = nullptr;
   }
 
   // move assignment operator
   Array4D &operator=(Array4D &&other) {
+    if (m_delete_buffer)
+      free(m_buffer);
     m_w_dim = other.m_w_dim;
-    m_x_dim = other.m_x_dim;
-    m_y_dim = other.m_y_dim;
     m_z_dim = other.m_z_dim;
+    m_y_dim = other.m_y_dim;
+    m_x_dim = other.m_x_dim;
+    m_delete_buffer = other.m_delete_buffer;
     m_buffer = other.m_buffer;
     other.m_buffer = nullptr;
     return *this;
   }
 
-  virtual ~Array4D() {}
+  virtual ~Array4D() {
+    if (m_delete_buffer)
+      free(m_buffer);
+  }
 
   T *data(size_t w = 0, size_t z = 0, size_t y = 0, size_t x = 0) const {
-    return &m_buffer.get()[x + m_x_dim * y + m_x_dim * m_y_dim * z +
-                           m_x_dim * m_y_dim * m_z_dim * w];
+    return &m_buffer[x + m_x_dim * y + m_x_dim * m_y_dim * z +
+                     m_x_dim * m_y_dim * m_z_dim * w];
   }
 
   size_t get_x_dim() const { return m_x_dim; }
@@ -321,23 +320,23 @@ public:
   size_t get_w_dim() const { return m_w_dim; }
 
   const T &operator()(size_t w, size_t z, size_t y, size_t x) const {
-    return m_buffer.get()[x + m_x_dim * y + m_x_dim * m_y_dim * z +
-                          m_x_dim * m_y_dim * m_z_dim * w];
+    return m_buffer[x + m_x_dim * y + m_x_dim * m_y_dim * z +
+                    m_x_dim * m_y_dim * m_z_dim * w];
   }
 
   T &operator()(size_t w, size_t z, size_t y, size_t x) {
-    return m_buffer.get()[x + m_x_dim * y + m_x_dim * m_y_dim * z +
-                          m_x_dim * m_y_dim * m_z_dim * w];
+    return m_buffer[x + m_x_dim * y + m_x_dim * m_y_dim * z +
+                    m_x_dim * m_y_dim * m_z_dim * w];
   }
 
   void init(const T &a) {
     const unsigned int n = m_x_dim * m_y_dim * m_z_dim * m_w_dim;
     for (unsigned int i = 0; i < n; ++i) {
-      m_buffer.get()[i] = a;
+      m_buffer[i] = a;
     }
   }
 
-  void zero() { memset((void *)m_buffer.get(), 0, bytes()); }
+  void zero() { memset((void *)m_buffer, 0, bytes()); }
 
   size_t size() const {
     return get_w_dim() * get_z_dim() * get_y_dim() * get_x_dim();
@@ -347,21 +346,18 @@ public:
     return get_w_dim() * get_z_dim() * get_y_dim() * get_x_dim() * sizeof(T);
   }
 
-  // TODO: if the buffer is not owned, there is no guarantee that it won't be
-  // destroyed. Need to return a copy in that case.
-  const std::shared_ptr<const T> get() const { return m_buffer; }
-
 protected:
+  size_t m_w_dim;
   size_t m_x_dim;
   size_t m_y_dim;
   size_t m_z_dim;
-  size_t m_w_dim;
-  std::shared_ptr<T> m_buffer;
+  bool m_delete_buffer;
+  T *m_buffer;
 };
 
 class Grid : public Array4D<std::complex<float>> {
 public:
-  Grid(Array3D<std::complex<float>> &array)
+  Grid(Array4D<std::complex<float>> &array)
       : Array4D<std::complex<float>>(array.data(), 1, array.get_z_dim(),
                                      array.get_y_dim(), array.get_x_dim()) {}
 
