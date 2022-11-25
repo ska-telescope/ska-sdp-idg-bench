1f261ecd0a9c22fffdc234c697ca7757afdc77bf
Stefano Corda <stefano.corda@epfl.ch>
2022-11-25 09:49:22 +0100

    update CUDA cmake

--- share/setup-env.sh
+++ share/setup-env.sh
@@ -1,3 +1,2 @@
-export BENCH_ROOT_DIR=$PWD
-export INSTALL_DIR=$BENCH_ROOT_DIR/install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR/lib:$BENCH_ROOT_DIR/external/libpowersensor/lib
+export BENCH_ROOT_DIR=${PWD}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BENCH_ROOT_DIR/install/lib:$BENCH_ROOT_DIR/external/libpowersensor/lib
--- share/setup-hip-nvidia.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_PATH=$CUDA_ROOT
-export HIP_PLATFORM=nvidia
-export HIP_COMPILER=nvcc
-export HIP_RUNTIME=cuda
-export HIPCC_COMPILE_FLAGS_APPEND="-std=c++11"
